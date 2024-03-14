2a48ae036c3ef16f6617f789f6175bbe5c060fab
ashiff30 <156168190+ashiff30@users.noreply.github.com>
2024-03-15 00:54:35 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,27 @@
+var productsContainer = document.getElementById("products");
+var search = document.getElementById("search");
+var productlist = productsContainer.querySelectorAll("div");
+
+search.addEventListener("keyup", function(){
+    var enteredValue = event.target.value.toUpperCase();
+
+    for(count=0;count<productlist.length;count=count+1){
+        var productname = productlist[count].querySelector("p").textContent;
+
+        if(productname.toUpperCase().indexOf(enteredValue)<0){
+            productlist[count].style.display="none";
+        } else {
+            productlist[count].style.display="block";
+        }
+    }
+})
+
+var sidenav=document.querySelector(".side-navbar");
+
+function showNavbar(){
+    sidenav.style.left="0";
+}
+
+function closeNavbar(){
+    sidenav.style.left="-60%";
+}
